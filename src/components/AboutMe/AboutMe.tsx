ba3f6e76f35040f87e1226c0a7ecb3cc11de55cc
Ruan Chagas <ruanchagas95@gmail.com>
2023-07-13 20:10:09 -0300

    style abouMe

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -3,18 +3,18 @@ function AboutMe() {
     const imgLink = 'https://media.licdn.com/dms/image/D5603AQEOGzgR6_Qmkg/profile-displayphoto-shrink_200_200/0/1683397874618?e=1694044800&v=beta&t=JUve6IgQYBqqYP6Ub61mPRtJ9jQbTKEMT4lEaMX-D00'
     return (
         <div className="container" id="about-me">
-        <div className="container-item" id="me-photo">
+        <div id="me-photo">
 
             <img className="photo" id="me-photo-jpg"
 
                 src={imgLink}
                 alt="Foto de perfil de Ruan Chagas" />
             <div id="me-name">
-                <h2><em>Ruan Chagas</em></h2>
+                <h5><em>Ruan Chagas</em></h5>
             </div>
         </div>
 
-        <div className="container-item" id="me-text">
+        <div id="me-text">
             <h3>
                 <p>Nascido no Rio de janeiro, moro atualmente em <span id='me-city'>Belford roxo, RJ</span>.
                     Sou apaixonado por tecnologias, Games e inteligências artificiais estão no meu dia a dia, mas sem
