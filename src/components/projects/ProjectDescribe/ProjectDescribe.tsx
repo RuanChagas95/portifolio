b71790d71c8d67bf03dbdc89c56e4707d52e7b8b
Ruan Chagas <ruanchagas95@gmail.com>
2023-07-17 16:55:28 -0300

    auto resize iframe

--- src/components/projects/ProjectDescribe/ProjectDescribe.tsx
+++ src/components/projects/ProjectDescribe/ProjectDescribe.tsx
@@ -1,17 +1,25 @@
 import { projectList } from "../projectList";
 import './projectDescribe.css'
 import { useParams } from "react-router-dom";
+import { useRef, useState } from "react";
 export default function ProjectDescribe() {
     const { project: folder } = useParams()
     const project = projectList.find((project) => project.folder === folder)
+    const projectRef = useRef<HTMLIFrameElement | null>(null)    
     if (!project){
-        return false
+        return null
+    }
+    const [height, setHeight] = useState(600)
+    const adjustHeigthSize = () => {
+        if (projectRef.current?.contentDocument) {           
+            setHeight(projectRef.current.contentDocument.documentElement.scrollHeight)
+        }
     }
     const {describe} = project
         return (<>
             <div className="container column">
             <div className="container text-center"><p>{describe}</p></div>
-            <iframe src={`/src/components/projects/${folder}/`}></iframe>
+            <iframe height={height + 'px'} onLoad={adjustHeigthSize} ref={projectRef} src={`/src/components/projects/${folder}/`}></iframe>
         </div>
         </>
     )
