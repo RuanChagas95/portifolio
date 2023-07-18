774321c32c42bf8ae6c47ddb1400b72d3f829920
Ruan Chagas <ruanchagas95@gmail.com>
2023-07-18 00:35:25 -0300

    add linkdin card

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const HeaderContainer = styled.header`
   display: flex;
@@ -36,7 +37,7 @@ function Header() {
   const linkedinPath = (
     <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
   );
-
+  const [linkedinIsVisible, setLinkedinIsVisible] = useState('none');
   return (
     <HeaderContainer>
       <Name>
@@ -45,14 +46,17 @@ function Header() {
         </Link>
       </Name>
       <StyledA href="https://github.com/RuanChagas95" target="_blank">
-        <SvgIcon style={{fill: '#000', padding: '1px', backgroundColor: 'white', borderRadius: '100%'}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+        <SvgIcon style={{ fill: '#000', padding: '1px', backgroundColor: 'white', borderRadius: '100%' }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
           {githubPath}
         </SvgIcon>
       </StyledA>
       <StyledA href="https://br.linkedin.com/in/ruan-chagas-626603272?trk=profile-badge" target="_blank">
-      <SvgIcon style={{backgroundColor: 'white', borderRadius: '3px', fill: 'rgb(0 83 167)'}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+        <SvgIcon
+          onMouseEnter={() => setLinkedinIsVisible('')} onMouseLeave={() => setLinkedinIsVisible('none')}
+          style={{position: 'relative', backgroundColor: 'white', borderRadius: '3px', fill: 'rgb(0 83 167)' }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
           {linkedinPath}
         </SvgIcon>
+      <div onMouseEnter={() => setLinkedinIsVisible('')} onMouseLeave={() => setLinkedinIsVisible('none')} style={{display: linkedinIsVisible, position: 'absolute'}} className="badge-base LI-profile-badge" data-locale="pt_BR" data-size="medium" data-theme="dark" data-type="HORIZONTAL" data-vanity="ruan-chagas-626603272" data-version="v1" ></div>
       </StyledA>
     </HeaderContainer>
   );
